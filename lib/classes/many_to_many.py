7d47f77555789fd30c83ee51d2c7e7a217f74da1
Igor Rakush <irakush@aol.com>
2024-01-18 22:47:42 -0500

    passed the test

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,34 +1,151 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 3 <= len(name) and not hasattr(self, "name"):
+            self._name = name
+
     def orders(self):
-        pass
-    
+        return [order for order in Order.all if order.coffee is self]
+
     def customers(self):
-        pass
-    
+        return list(set([order.customer for order in self.orders()]))
+
     def num_orders(self):
-        pass
-    
+        return len(self.orders())
+
     def average_price(self):
-        pass
+        total_cost = sum([order.price for order in Order.all if order.coffee is self])
+        return total_cost / self.num_orders()
+
 
 class Customer:
+    all = []
+
     def __init__(self, name):
         self.name = name
-        
+        Customer.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 15:
+            self._name = name
+
     def orders(self):
-        pass
-    
+        return [order for order in Order.all if order.customer is self]
+
     def coffees(self):
-        pass
-    
+        return list(set([order.coffee for order in self.orders()]))
+
     def create_order(self, coffee, price):
-        pass
-    
+        return Order(self, coffee, price)
+
+    def total_payed(self):
+        return sum([order.price for order in self.orders()])
+
+    def payd_by_coffee(self, coffee):
+        return sum([order.price for order in Order.all if order.coffee is coffee])
+
+    @classmethod
+    def all_customers(cls):
+        print([customer.name for customer in cls.all])
+
+    @classmethod
+    def all_payed(cls, coffee, customer):
+        return [
+            order.price
+            for order in Order.all
+            if order.customer is customer and order.coffee is coffee
+        ]
+
+    # @classmethod
+    # def most_payd(cls, coffee):
+    #     customers_dict = []
+
+    #     for each_c in cls.all:
+    #         customers_dict.append({each_c.name: sum(cls.all_payed(coffee, each_c))})
+
+    #     max_payed = 0
+    #     customer_payed = ""
+    #     for obj in customers_dict:
+    #         for key in obj.keys():
+    #             if obj[key] > max_payed:
+    #                 max_payed = obj[key]
+    #                 customer_payed = key
+
+    #     print(customers_dict)
+    #     print(customer_payed)
+
+    @classmethod
+    def get_customer_by_name(cls, customer_name):
+        return [customer for customer in cls.all if customer.name == customer_name]
+
+    @classmethod
+    def most_aficionado(cls, coffee):
+        customers_dict = []
+
+        for each_c in cls.all:
+            customers_dict.append({each_c.name: sum(cls.all_payed(coffee, each_c))})
+
+        max_payed = 0
+        customer_payed = ""
+        for obj in customers_dict:
+            for key in obj.keys():
+                if obj[key] > max_payed:
+                    max_payed = obj[key]
+                    customer_payed = key
+
+        print(customers_dict)
+        print(customer_payed)
+        return cls.get_customer_by_name(customer_payed)
+
+
 class Order:
+    all = []
+
     def __init__(self, customer, coffee, price):
         self.customer = customer
         self.coffee = coffee
         self.price = price
+        Order.all.append(self)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, price):
+        if (
+            isinstance(price, float)
+            and 1.0 <= price <= 10.0
+            and not hasattr(self, "price")
+        ):
+            self._price = price
+
+    @property
+    def customer(self):
+        return self._customer
+
+    @customer.setter
+    def customer(self, customer):
+        if isinstance(customer, Customer):
+            self._customer = customer
+
+    @property
+    def coffee(self):
+        return self._coffee
+
+    @coffee.setter
+    def coffee(self, coffee):
+        if isinstance(coffee, Coffee):
+            self._coffee = coffee
--- lib/debug.py
+++ lib/debug.py
@@ -5,7 +5,35 @@ from classes.many_to_many import Customer
 from classes.many_to_many import Order
 from classes.many_to_many import Coffee
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("HELLO! :) let's debug")
+
+    coffee_1 = Coffee("Americano")
+    coffee_2 = Coffee("Latte")
+
+    customer_1 = Customer("John")
+    customer_2 = Customer("Anna")
+    customer_3 = Customer("Boris")
+
+    order_1 = Order(customer_1, coffee_1, 2.0)
+    order_2 = Order(customer_2, coffee_1, 5.0)
+    order_3 = Order(customer_1, coffee_2, 5.0)
+    order_4 = Order(customer_3, coffee_2, 3.0)
+    order_2 = Order(customer_1, coffee_1, 5.0)
+
+    ##########################################
+    ##########################################
+    ############### * TOTAL * ################
+    ##########################################
+    ##########################################
+    all_customers = [customer_1, customer_2, customer_3]
+
+    def total():
+        total = 0
+        for each_c in all_customers:
+            total += each_c.total_payed()
+            print(f"Customer: {each_c.name} :: Total Payed: {each_c.total_payed()}")
+
+        print(f"Total: {total}")
 
     ipdb.set_trace()
